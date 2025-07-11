d885bd5d10a97e7876e3391abd62d621f4a909dc
zenthus <4B1L0901@stust.edu.tw>
2025-07-11 20:12:59 +0800

    hyprpaper config done

--- user/zenthus/hypr/hyprpaper.nix
+++ user/zenthus/hypr/hyprpaper.nix
@@ -1,16 +1,16 @@
 {...}:
 {
-  programs.hyprpaper = {
+  services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "off";    #要不要集成轉場
       splash = false; # 轉場效果
       splash_offset = 2.0;
       
-      preload = [ "../wallpapers/Lofi-Computer.png" ];
-      wallpapaer = [
-        "DP-1, ../wallpapers/Lofi-Computer.png"
-        "DP-2, ../wallpapers/Lofi-Computer.png"
+      preload = [ "/home/zenthus/zix/wallpapers/Lofi-Computer.png" ];
+      wallpaper = [
+        "DP-1, /home/zenthus/zix/wallpapers/Lofi-Computer.png"
+        "DP-2, /home/zenthus/zix/wallpapers/Lofi-Computer.png"
       ];
     };
 
